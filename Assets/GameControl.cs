2de5a7e4e9926ab7939331f363fb180c9ca29e08
Afşin Loğoğlu <muhammedafsin@hotmail.com>
2020-08-01 19:31:13 +0300

    First Commit
    initial commit

---
+++ Assets/GameControl.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject backgroundOne;
+    public GameObject backgroundTwo;
+
+    public float backgroundSpeed = -1.5f;
+
+    Rigidbody2D physicsOne;
+    Rigidbody2D physicsTwo;
+    Rigidbody2D physicsThree;
+
+    float lenght = 0;
+
+    public GameObject Pipe;
+    public int pipeCounter;
+    GameObject []pipes;
+    public int pipeCounter2=0;
+
+    float timeChange;
+    bool endGame = true;
+
+    void Start()
+    {
+        physicsOne = backgroundOne.GetComponent<Rigidbody2D>();
+        physicsTwo = backgroundTwo.GetComponent<Rigidbody2D>();
+        
+
+        physicsOne.velocity = new Vector2(backgroundSpeed, 0);
+        physicsTwo.velocity = new Vector2(backgroundSpeed, 0);
+
+        lenght = backgroundOne.GetComponent<BoxCollider2D>().size.x;
+
+        pipes = new GameObject[pipeCounter];
+
+
+        for (int i = 0; i < pipes.Length; i++)
+        {
+            pipes[i] = Instantiate(Pipe, new Vector2(-20, -20), Quaternion.identity);
+            physicsThree = pipes[i].GetComponent<Rigidbody2D>();
+            physicsThree.velocity = new Vector2(backgroundSpeed, 0);
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (endGame)
+        {
+
+            if (backgroundOne.transform.position.x <= - lenght)
+            backgroundOne.transform.position += new Vector3(lenght * 2, 0);
+
+            if (backgroundTwo.transform.position.x <= - lenght)
+            backgroundTwo.transform.position += new Vector3(lenght * 2, 0);
+
+        //------------------pipes spawning---------------------------
+
+        timeChange += Time.deltaTime;
+            if (timeChange > 2f)
+             {
+                timeChange = 0;
+            float yAxis = Random.Range(-0.95f,-3.45f);
+            pipes[pipeCounter2].transform.position = new Vector3(6, yAxis);
+            pipeCounter2++;
+            if (pipeCounter2 >= pipes.Length)
+                pipeCounter2 = 0;
+             }
+        }
+        
+            
+
+
+
+    }
+
+    public void gameOver()
+    {
+
+
+        for (int i = 0; i < pipes.Length; i++)
+        {
+            pipes[i].GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            physicsOne.velocity = Vector2.zero;
+            physicsTwo.velocity = Vector2.zero;
+            physicsThree.velocity = Vector2.zero;
+        }
+        endGame = false;
+
+    }
+}
---
+++ Assets/Scripts/BirdControl.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BirdControl : MonoBehaviour
+{
+    public Sprite []birdSprite; //kus animasyonu icin dizi olusturuluyor
+    SpriteRenderer spriteRenderer;
+    bool movingControl = true;
+    int birdCounter = 0;
+    float birdAnimSpeed = 0;
+    int point = 0;
+    Rigidbody2D physics;
+    bool gameOver=true;
+
+    public Text scoreText;
+
+    GameControl gameControl;
+
+    AudioSource []sounds;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        physics = GetComponent<Rigidbody2D>();
+        gameControl = GameObject.FindGameObjectWithTag("GameControl").GetComponent<GameControl>();
+        sounds = GetComponents<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0) && gameOver==true)
+        {
+            physics.velocity = new Vector2(0, 0);  //hızı sıfır yaptık ondan sonra yükseliyor(yercekimine karsi)
+            physics.AddForce(new Vector2(0,200));
+            sounds[0].Play();
+            
+        }
+        if (physics.velocity.y>0)
+        {
+            transform.eulerAngles = new Vector3(0, 0, 40);
+
+        }
+        else
+        {
+            transform.eulerAngles = new Vector3(0, 0, -40);
+
+        }
+        Animation();
+    }      
+
+    void Animation()
+    {
+        birdAnimSpeed += Time.deltaTime;
+
+        if (birdAnimSpeed > .1f)
+        {
+            birdAnimSpeed = 0;
+            if (movingControl)
+            {
+                spriteRenderer.sprite = birdSprite[birdCounter];
+                birdCounter++;
+                if (birdCounter == birdSprite.Length)
+                {
+                    birdCounter--;
+                    movingControl = false;
+                }
+
+            }
+            else
+            {
+                birdCounter--;
+                spriteRenderer.sprite = birdSprite[birdCounter];
+                if (birdCounter == 0)
+                {
+                    birdCounter++;
+                    movingControl = true;
+                }
+
+
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Point")
+        {
+            point++;
+            scoreText.text = "Score = " + point;
+            Destroy(GameObject.FindGameObjectWithTag("Point"));
+            sounds[1].Play();
+            
+            
+
+        }
+        if (col.gameObject.tag == "Pipe")
+        {
+            gameOver = false;
+            gameControl.gameOver();
+            sounds[2].Play();
+            GetComponent<CircleCollider2D>().enabled = false;
+            
+
+        }
+
+    }
+    
+}
